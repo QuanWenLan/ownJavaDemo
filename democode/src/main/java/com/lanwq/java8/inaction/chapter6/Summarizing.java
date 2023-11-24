5e8af929a9e12de5824af7a5ec0782dd7f180481
vin <670453221@qq.com>
2023-11-24 17:31:47 +0800

    java8实战代码

---
+++ democode/src/main/java/com/lanwq/java8/inaction/chapter6/Summarizing.java
@@ -0,0 +1,69 @@
+package com.lanwq.java8.inaction.chapter6;
+
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+import java.util.function.BinaryOperator;
+
+import static com.lanwq.java8.inaction.chapter6.Dish.menu;
+import static java.util.stream.Collectors.*;
+
+/**
+ * @author Lan
+ * @createTime 2023-11-23  17:08
+ **/
+public class Summarizing {
+    public static void main(String... args) {
+        System.out.println("Nr. of dishes: " + howManyDishes());
+        System.out.println("The most caloric dish is: " + findMostCaloricDish());
+        System.out.println("The most caloric dish is: " + findMostCaloricDishUsingComparator());
+        System.out.println("Total calories in menu: " + calculateTotalCalories());
+        System.out.println("Average calories in menu: " + calculateAverageCalories());
+        System.out.println("Menu statistics: " + calculateMenuStatistics());
+        System.out.println("Short menu: " + getShortMenu());
+        System.out.println("Short menu comma separated: " + getShortMenuCommaSeparated());
+    }
+
+
+    private static long howManyDishes() {
+        return menu.stream().collect(counting());
+    }
+
+    private static Dish findMostCaloricDish() {
+        return menu.stream().collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)).get();
+    }
+
+    private static Dish findMostCaloricDishUsingComparator() {
+        Comparator<Dish> dishCaloriesComparator = Comparator.comparingInt(Dish::getCalories);
+        BinaryOperator<Dish> moreCaloricOf = BinaryOperator.maxBy(dishCaloriesComparator);
+        return menu.stream().collect(reducing(moreCaloricOf)).get();
+    }
+
+    private static int calculateTotalCalories2() {
+//        int totalCalories = menu.stream().collect(reducing(0, Dish::getCalories, (i, j) -> i + j));
+        // or optimize
+        int totalCalories = menu.stream().collect(reducing(0,
+                Dish::getCalories,
+                Integer::sum));
+        return totalCalories;
+    }
+
+    private static int calculateTotalCalories() {
+        return menu.stream().collect(summingInt(Dish::getCalories));
+    }
+
+    private static Double calculateAverageCalories() {
+        return menu.stream().collect(averagingInt(Dish::getCalories));
+    }
+
+    private static IntSummaryStatistics calculateMenuStatistics() {
+        return menu.stream().collect(summarizingInt(Dish::getCalories));
+    }
+
+    private static String getShortMenu() {
+        return menu.stream().map(Dish::getName).collect(joining());
+    }
+
+    private static String getShortMenuCommaSeparated() {
+        return menu.stream().map(Dish::getName).collect(joining(", "));
+    }
+}
