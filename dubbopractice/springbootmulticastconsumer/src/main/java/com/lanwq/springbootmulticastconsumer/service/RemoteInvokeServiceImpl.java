88fa512ef43c2b4549538e91beb94ae2e03d7ce7
vin <670453221@qq.com>
2024-03-05 17:28:50 +0800

    添加dubbo服务端、消费端的属性配置注释代码

--- dubbopractice/springbootmulticastconsumer/src/main/java/com/lanwq/springbootmulticastconsumer/service/RemoteInvokeServiceImpl.java
+++ dubbopractice/springbootmulticastconsumer/src/main/java/com/lanwq/springbootmulticastconsumer/service/RemoteInvokeServiceImpl.java
@@ -7,6 +7,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class RemoteInvokeServiceImpl {
 
+    /**
+     * 在这边也有其他的一些属性可以进行设置
+     * 服务引用配置：
+     * @Reference(interfaceClass = YourService.class)：指定引用的服务接口。
+     * 超时配置：
+     * @Reference(timeout = 3000)：设置服务引用的超时时间（毫秒）。
+     * 版本配置：
+     * @Reference(version = "1.0")：指定引用的服务版本号。
+     * 负载均衡配置：
+     * @Reference(loadbalance = "roundrobin")：设置负载均衡策略，例如 roundrobin、random、leastactive 等。
+     */
     // , parameters = {"unicast", "false"} 需要设置这个参数，在配置文件里设置没有用！！！麻痹的
     @Reference(timeout = 2000, parameters = {"unicast", "false"})
     private DemoService demoService;
